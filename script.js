ca196102bfad31f6030ea5c551b29fa55fdd7b10
Minho Kim <music4537@naver.com>
2022-07-26 15:03:00 +0900

    map scroll controller

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*
+1. 지동 생성 & 확대 축소 컨트롤러
+2. 더미데이터 준비하기 (제목, 주소, url, 카테고리)
+3. 여러개 마커 찍기
+    * 주소 - 좌표 변환 (지도 라이브러리)
+4. 마커에 인포윈도우 붙이기
+    *마커에 클릭 이벤트로 인포윈도우
+    *url 에서 섬네일 따기
+    *클릭한 마커로 지도 센터 이동
+5. 카테고리 분류
+*/
+
+// 1. 지도 생성 & 확대 축소 컨트롤러
+
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
@@ -5,3 +19,22 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
